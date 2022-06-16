2c4437dc368f2349daf6c4c57a9ed92c85d50d8a
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-06-16 13:58:02 +0200

    ho critto l'html e impostato il ciclo

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+fizz 3 buzz 5 fizzbuzz 15
+-imposto delle variabili
+-imposto un ciclo for con un index da 1 a 100
+-imposto degli if nel ciclo per visualizzare fizz buzz e fizzbuzz
+-stampo in console
+ */
+for (let index = 1; index <= 100 ; index++) {
+
+    const number = document.createElement('div')
+}
