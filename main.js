896e64d3470b75ea8f6264843a56cf684bfc5441
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-06-16 14:08:09 +0200

    ho fatto le box con i numeri da 1 a 100 e le ho stylate

--- main.js
+++ main.js
@@ -5,7 +5,15 @@ fizz 3 buzz 5 fizzbuzz 15
 -imposto degli if nel ciclo per visualizzare fizz buzz e fizzbuzz
 -stampo in console
  */
+const numberRow = document.getElementById('number');
+
 for (let index = 1; index <= 100 ; index++) {
 
-    const number = document.createElement('div')
+    const numberRowBox = document.createElement('div');
+
+    numberRowBox.classList.add('box','col-2');
+
+    numberRowBox.innerHTML = index;
+
+    numberRow.append(numberRowBox);
 }
